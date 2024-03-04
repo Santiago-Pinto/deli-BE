9fcff33c9cb10d2671cefe73397ef39b2ce3fcf5
Santiago <santiago.pinto@digitalbeings.net>
2024-03-04 13:52:26 -0300

    Added cors dependency

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express, { Application, json } from "express";
 import { AccountsRouter } from "./api/routes/accounts";
+import cors from "cors";
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 // Here go the routers
 app.use(json());
+app.use(cors());
+
 app.use("/accounts", AccountsRouter);
 
 if (process.env.NODE_ENV !== "test") {
